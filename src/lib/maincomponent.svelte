597c1b7cf713e59f68dd05d287428c8920234b68
AhmedHecky <147705662+AhmedHecky@users.noreply.github.com>
2025-01-31 20:35:05 +0000

    added a terminal to the svelte file. next i need to think of a theme and push the changes to the main branch

--- src/lib/maincomponent.svelte
+++
@@ -1,3 +0,0 @@
-<p class="flex justify-center items-center font-mono">
-    consolas font applies here
-</p>
---
+++ src/lib/terminal.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {TypeWriter} from 'svelte-typewrite';
+</script>
+<div class="flex justify-start items-start bg-black font-mono text-green-500 h-screen w-screen p-5" >
+    <span class="text-red-500">root</span>@portfolio: <span class="text-white"><TypeWriter texts={[' #under devleopment']} typeSpeed={100} /></span>
+</div>
