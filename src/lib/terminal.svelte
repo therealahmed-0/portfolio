b650f4cc56a387c34167dc089d5bbdabe7ebd873
AhmedHecky <147705662+AhmedHecky@users.noreply.github.com>
2025-02-01 11:52:03 +0000

    changed favicon and added glass panel

--- src/lib/terminal.svelte
+++ src/lib/terminal.svelte
@@ -1,7 +1,7 @@
 <script>
     import {TypeWriter} from 'svelte-typewrite';
 </script>
-<div class="flex justify-start items-start bg-gray-950 font-mono text-green-500 h-screen w-screen p-5" >
+<div class="flex justify-start items-start bg-gray-950 font-mono text-green-500 h-screen w-full p-5" >
    <div class="bg-white/10 backdrop-blur-lg border border-white/20 shadow-lg rounded-xl p-6 max-w-md mx-auto">
         <div class="flex space-x-2 p-2rounded-t-lg w-screen">
             <div class="w-3 h-3 rounded-full bg-red-500"></div>
