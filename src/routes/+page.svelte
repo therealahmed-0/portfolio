03e02614ef049ff505e14afd6d556ecf2c2d89c3
AhmedHecky <147705662+AhmedHecky@users.noreply.github.com>
2025-03-31 22:07:54 +0000

    completed about me section and tech-stack line and made sure to fit legal requirements with company logos. still need to address the logo issue as not working on many devices.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,100 +1,110 @@
 <script lang="ts">
-    import Navbar from "../lib/Navbar.svelte";
-    import { TypeWriter } from 'svelte-typewrite';
+  import Navbar from "../lib/Navbar.svelte";
+  import { TypeWriter } from 'svelte-typewrite';
 
-    let quotes = [
-        "The world doesn't care about your excuses. All that matters is the result.",
-        "Winners don't watch the clock. They work until the job is done.",
-        "If you want extraordinary results, you have to put in extraordinary efforts.",
-        "If failure makes you stronger, then you can never lose.",
-        "Discipline is not a punishment, it's a practice of self-control and mastery.",
-        "Success comes to those who are willing to risk for it.",
-        "The amount of stress is the level of success you can enjoy.",
-        "Do the impossible, and you will never doubt yourself again.",
-        "The only thing holding you back is your own mindset.",
-        "You must put in the effort to live the life you want.",
-        "Success isn't about what you accomplish, it's about what you inspire others to do.",
-        "Fear is the enemy of success; conquer it and you will conquer anything."
-    ];
+  let quotes = [
+      "The world doesn't care about your excuses. All that matters is the result.",
+      "Winners don't watch the clock. They work until the job is done.",
+      "If you want extraordinary results, you have to put in extraordinary efforts.",
+      "If failure makes you stronger, then you can never lose.",
+      "Discipline is not a punishment, it's a practice of self-control and mastery.",
+      "Success comes to those who are willing to risk for it.",
+      "The amount of stress is the level of success you can enjoy.",
+      "Do the impossible, and you will never doubt yourself again.",
+      "The only thing holding you back is your own mindset.",
+      "You must put in the effort to live the life you want.",
+      "Success isn't about what you accomplish, it's about what you inspire others to do.",
+      "Fear is the enemy of success; conquer it and you will conquer anything."
+  ];
 
-    let selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  let selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-    function changeQuote() {
-        let newQuote;
-        do {
-            newQuote = quotes[Math.floor(Math.random() * quotes.length)];
-        } while (newQuote === selectedQuote);
-        selectedQuote = newQuote;
-    }
+  function changeQuote() {
+      let newQuote;
+      do {
+          newQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      } while (newQuote === selectedQuote);
+      selectedQuote = newQuote;
+  }
 </script>
-<div class=" sticky top-0">
-    <Navbar />
-</div>
-<div class="flex flex-col h-screen">
-        <div class="flex flex-grow justify-center items-center">
-            <div class="text-center flex flex-col items-center justify-center">
-                <span class="bg-gradient-to-b from-red-500 to-red-900 bg-clip-text text-transparent text-xl md:text-2xl lg:text-5xl font-bold p-5">
-                    <TypeWriter 
-                        texts={[`"${selectedQuote}"`]} 
-                        ondeleteend={changeQuote}
-                    />
-                </span>
-                <div class="p-5">
-                    <span class="text-gray-600 text-[12px] md:text-[24px] lg:text-lg">
-                        Scroll down to learn more
-                    </span>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <div class="flex flex-col h-max bg-gray-950" id="About">
-        <div class="p-15 m-3">
-            <h2 class="bg-gradient-to-b from-black via-gray-800 to-white bg-clip-text text-transparent font-semibold text-4xl md:text-7xl lg:text-10xl p-4 md:p-8 lg:p-12 ">
-                About me
-           </h2>
-           <p class="text-gray-300 text-xl md:text-2xl lg:text-3xl p-3 md:p-6 lg:p-9">
-            Hi, my name is Ahmed. I’m an aspiring computer science student with strengths in full-stack development, problem-solving, and system design. Currently, I’m learning machine learning with Python to build scalable real-world solutions. Beyond computer science, I’m dedicated to academics and sports, particularly mathematics and martial arts. My goal is to push my limits, develop innovative projects, and contribute meaningfully to the tech community.
-           </p>
-        </div>
-    </div>
-    <br>
-    <div class="flex justify-center items-center text-2xl md:text-3xl lg:text-4xl bg-gradient-to-b from-red-500 to-red-950 bg-clip-text text-transparent font-semibold">
-        Tech Stack
-    </div>
-    <div class="w-full h-screen md:h-max lg:h-max bg-gray-950 text-white flex flex-col md:flex-row justify-center items-center gap-8 p-4">
-        <!-- SvelteKit -->
-        <div class="flex flex-col justify-center items-center text-center p-4 w-full">
-          <a href="https://svelte.dev" aria-label="svelte">
-            <svg class="w-24 h-24 sm:w-32 sm:h-32 md:w-20 md:h-20 lg:w-24 lg:h-24" viewBox="0 0 24 24" fill="#ff0000" xmlns="http://www.w3.org/2000/svg">
-              <path d="M20.68 3.17a7.3 7.3 0 0 0-9.8-2.1l-5.6 3.56a6.36 6.36 0 0 0-2.89 4.3 6.66 6.66 0 0 0 .67 4.33 6.14 6.14 0 0 0-.95 2.4 6.84 6.84 0 0 0 1.16 5.16 7.33 7.33 0 0 0 9.8 2.12l5.6-3.56a6.36 6.36 0 0 0 2.88-4.3 6.66 6.66 0 0 0-.67-4.32 6.79 6.79 0 0 0-.2-7.59zM10.32 21.13a4.43 4.43 0 0 1-4.76-1.77c-.65-.9-.89-2.01-.7-3.11l.11-.53.1-.33.3.2c.66.5 1.4.86 2.19 1.1l.2.07-.02.2c-.02.28.06.59.22.83.33.47.9.7 1.45.55.12-.04.24-.08.34-.14l5.58-3.56c.28-.18.46-.45.53-.77.06-.33-.02-.67-.2-.94-.33-.46-.9-.67-1.45-.53-.12.04-.25.09-.35.15l-2.11 1.34a4.43 4.43 0 0 1-5.9-1.28 4.1 4.1 0 0 1-.7-3.11A3.85 3.85 0 0 1 6.92 6.9l5.57-3.56c.35-.22.73-.38 1.14-.5 1.8-.47 3.7.24 4.76 1.76a4.12 4.12 0 0 1 .57 3.64l-.1.33-.29-.2a7.42 7.42 0 0 0-2.2-1.1l-.2-.06.02-.2c.02-.29-.06-.6-.22-.84-.33-.47-.9-.67-1.45-.53-.12.04-.24.08-.34.14L8.59 9.37c-.28.19-.46.45-.52.78-.06.32.02.67.2.93.32.47.9.67 1.44.53.13-.04.25-.08.35-.14l2.13-1.36c.35-.23.74-.4 1.14-.51 1.81-.47 3.7.24 4.76 1.77.65.9.9 2.01.72 3.1a3.85 3.85 0 0 1-1.75 2.6l-5.58 3.55a4.9 4.9 0 0 1-1.16.51z"/>
-            </svg>
-          </a>
-          <span class="text-3xl sm:text-4xl md:text-2xl lg:text-3xl">SvelteKit</span>
-        </div>
-      
-        <!-- Firebase -->
-        <div class="flex flex-col justify-center items-center text-center p-4 w-full">
-          <a href="https://firebase.google.com" aria-label="firebase">
-            <svg class="w-24 h-24 sm:w-32 sm:h-32 md:w-20 md:h-20 lg:w-24 lg:h-24" viewBox="0 0 24 24" fill="#ff0000" xmlns="http://www.w3.org/2000/svg">
-              <path d="M3.89 15.673L6.255.461A.542.542 0 0 1 7.27.289L9.813 5.06 3.89 15.673zm16.795 3.691L18.433 5.365a.543.543 0 0 0-.918-.295l-14.2 14.294 7.857 4.428a1.62 1.62 0 0 0 1.587 0l7.926-4.428zM14.3 7.148l-1.82-3.482a.542.542 0 0 0-.96 0L3.53 17.984 14.3 7.148z"/>
-            </svg>
-          </a>
-          <span class="text-3xl sm:text-4xl md:text-2xl lg:text-3xl">Firebase</span>
-        </div>
-      
-        <!-- Tailwind -->
-        <div class="flex flex-col justify-center items-center text-center p-4 w-full">
-          <a href="https://v2.tailwindcss.com" aria-label="tailwind">
-            <svg class="w-24 h-24 sm:w-32 sm:h-32 md:w-20 md:h-20 lg:w-24 lg:h-24" viewBox="0 0 24 24" fill="#ff0000" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z"/>
-            </svg>
-          </a>
-          <span class="text-3xl sm:text-4xl md:text-2xl lg:text-3xl">Tailwind</span>
-        </div>
-      </div>
-      
-    <div class="h-screen">
-    
-    </div>    
 
+<div class="fixed top-0 left-0 w-full z-50">
+  <Navbar />
+</div>
+
+<main>
+  <div class="flex flex-col h-screen justify-center items-center text-center">
+      <span class="bg-gradient-to-b from-red-500 to-red-900 bg-clip-text text-transparent text-xl md:text-2xl lg:text-5xl font-bold p-5">
+          <TypeWriter 
+              texts={[`"${selectedQuote}"`]} 
+              ondeleteend={changeQuote}
+          />
+      </span>
+      <div class="p-5">
+          <span class="text-gray-600 text-[12px] md:text-[24px] lg:text-lg">
+              Scroll down to learn more
+          </span>
+      </div>
+  </div>
+
+  <section class="flex flex-col items-center justify-center h-screen pt-24 md:pt-32 lg:pt-36 px-6 md:px-12 text-center">
+    <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold bg-gradient-to-b from-white to-slate-950 text-transparent bg-clip-text">About Me</h1>
+    <p class="text-gray-300 text-lg md:text-xl lg:text-2xl leading-relaxed max-w-3xl mt-4 md:mt-6">
+        Hey, I'm <span class="text-red-400 font-semibold">Ahmed</span>, a passionate 
+        <span class="text-blue-400">computer science enthusiast</span> specializing in 
+        <span class="text-green-400">full-stack development, system design, and problem-solving</span>. 
+        Currently, I'm exploring <span class="text-yellow-400">many fields in the world of software and physical engineering</span> to build scalable, real-world solutions.
+    </p>
+    <p class="text-gray-300 text-lg md:text-xl lg:text-2xl max-w-3xl mt-4">
+        Beyond coding, I'm dedicated to <span class="text-purple-400">mathematics and physics</span>, pushing my limits through academic challenges. 
+        I also train in <span class="text-orange-400">martial arts</span>, valuing discipline, resilience, and continuous growth.
+    </p>
+    <p class="text-gray-300 text-lg md:text-xl lg:text-2xl max-w-3xl mt-4">
+        My goal? To <span class="text-pink-400">create impactful projects</span>, explore the 
+        <span class="text-teal-400">frontiers of technology</span>, and contribute to the global tech community. 🚀
+    </p>
+</section>
+  <br>
+  <br>
+  <br>
+  <div class="flex justify-center items-center text-3xl md:text-4xl lg:text-5xl bg-gradient-to-b from-red-500 to-red-950 bg-clip-text text-transparent font-semibold">
+    Tech Stack
+  </div>
+  <div class="w-full h-screen md:h-max lg:h-max bg-gray-950 text-white flex flex-col md:flex-row justify-center items-center gap-8 p-4">
+    <div class="flex flex-col justify-center items-center text-center p-4 w-full">
+      <a href="https://svelte.dev" aria-label="svelte">
+        <svg class="w-24 h-24 aspect-square" viewBox="0 0 98.1 118" xmlns="http://www.w3.org/2000/svg">
+          <path d="M91.8 15.6C80.9-0.1 59.2-4.7 43.6 5.2L16.1 22.8C8.6 27.5 3.4 35.2 1.9 43.9c-1.3 7.3-0.2 14.8 3.3 21.3-2.4 3.6-4 7.6-4.7 11.8-1.6 8.9 0.5 18.1 5.7 25.4 11 15.7 32.6 20.3 48.2 10.4l27.5-17.5c7.5-4.7 12.7-12.4 14.2-21.1 1.3-7.3 0.2-14.8-3.3-21.3 2.4-3.6 4-7.6 4.7-11.8 1.7-9-0.4-18.2-5.7-25.5" fill="#ff3e00"/>
+          <path d="M40.9 103.9c-8.9 2.3-18.2-1.2-23.4-8.7-3.2-4.4-4.4-9.9-3.5-15.3 0.2-0.9 0.4-1.7 0.6-2.6l0.5-1.6 1.4 1c3.3 2.4 6.9 4.2 10.8 5.4l1 0.3-0.1 1c-0.1 1.4 0.3 2.9 1.1 4.1 1.6 2.3 4.4 3.4 7.1 2.7 0.6-0.2 1.2-0.4 1.7-0.7L65.5 72c1.4-0.9 2.3-2.2 2.6-3.8 0.3-1.6-0.1-3.3-1-4.6-1.6-2.3-4.4-3.3-7.1-2.6-0.6 0.2-1.2 0.4-1.7 0.7l-10.5 6.7c-1.7 1.1-3.6 1.9-5.6 2.4-8.9 2.3-18.2-1.2-23.4-8.7-3.1-4.4-4.4-9.9-3.4-15.3 0.9-5.2 4.1-9.9 8.6-12.7l27.5-17.5c1.7-1.1 3.6-1.9 5.6-2.5 8.9-2.3 18.2 1.2 23.4 8.7 3.2 4.4 4.4 9.9 3.5 15.3-0.2 0.9-0.4 1.7-0.7 2.6l-0.5 1.6-1.4-1c-3.3-2.4-6.9-4.2-10.8-5.4l-1-0.3 0.1-1c0.1-1.4-0.3-2.9-1.1-4.1-1.6-2.3-4.4-3.3-7.1-2.6-0.6 0.2-1.2 0.4-1.7 0.7L32.4 46.1c-1.4 0.9-2.3 2.2-2.6 3.8s0.1 3.3 1 4.6c1.6 2.3 4.4 3.3 7.1 2.6 0.6-0.2 1.2-0.4 1.7-0.7l10.5-6.7c1.7-1.1 3.6-1.9 5.6-2.5 8.9-2.3 18.2 1.2 23.4 8.7 3.2 4.4 4.4 9.9 3.5 15.3-0.9 5.2-4.1 9.9-8.6 12.7l-27.5 17.5c-1.7 1.1-3.6 1.9-5.6 2.5" fill="#ffffff"/>
+        </svg>
+      </a>
+      <span>
+        svelte
+      </span>
+    </div>
+    <div class="flex flex-col justify-center items-center text-center p-4 w-full">
+      <a href="https://firebase.google.com" aria-label="firebase">
+        <svg class="w-24 h-24 aspect-square" viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M213.918 560.499C237.166 569.856 262.387 575.408 288.87 576.333C324.71 577.585 358.792 570.175 389.261 556.099C352.724 541.744 319.634 520.751 291.392 494.651C273.086 523.961 246.01 547.113 213.918 560.499Z" fill="#FF9100"/>
+          <path d="M291.389 494.66C226.923 435.038 187.815 348.743 191.12 254.092C191.228 251.019 191.39 247.947 191.58 244.876C180.034 241.89 167.98 240.068 155.576 239.635C137.821 239.015 120.626 241.217 104.393 245.788C87.1838 275.933 76.7989 310.521 75.5051 347.569C72.1663 443.18 130.027 526.723 213.914 560.508C246.007 547.121 273.082 523.998 291.389 494.66Z" fill="#FFC400"/>
+          <path d="M291.39 494.657C306.378 470.671 315.465 442.551 316.523 412.254C319.306 332.559 265.731 264.003 191.581 244.873C191.391 247.944 191.229 251.016 191.121 254.089C187.816 348.74 226.924 435.035 291.39 494.657Z" fill="#FF9100"/>
+          <path d="M308.231 20.8584C266 54.6908 232.652 99.302 212.475 150.693C200.924 180.129 193.665 211.748 191.546 244.893C265.696 264.023 319.272 332.579 316.489 412.273C315.431 442.57 306.317 470.663 291.355 494.677C319.595 520.804 352.686 541.77 389.223 556.124C462.56 522.224 514.593 449.278 517.606 362.997C519.558 307.096 498.08 257.273 467.731 215.219C435.68 170.742 308.231 20.8584 308.231 20.8584Z" fill="#DD2C00"/>
+        </svg>
+      </a>
+      <span>
+        Firebase
+      </span>
+    </div>
+    <div class="flex flex-col justify-center items-center text-center p-4 w-full">
+      <a href="https://tailwindcss.com" aria-label="tailwind">
+        <svg class="w-24 h-24 aspect-square" viewBox="0 0 256 154" xmlns="http://www.w3.org/2000/svg">
+          <path d="M128 0C93.867 0 72.533 17.067 64 51.2 76.8 34.133 91.733 27.733 108.8 32c9.737 2.434 16.697 9.499 24.401 17.318C145.751 62.057 160.275 76.8 192 76.8c34.133 0 55.467-17.067 64-51.2-12.8 17.067-27.733 23.467-44.8 19.2-9.737-2.434-16.697-9.499-24.401-17.318C174.249 14.743 159.725 0 128 0zM64 76.8C29.867 76.8 8.533 93.867 0 128c12.8-17.067 27.733-23.467 44.8-19.2 9.737 2.434 16.697 9.499 24.401 17.318C81.751 138.857 96.275 153.6 128 153.6c34.133 0 55.467-17.067 64-51.2-12.8 17.067-27.733 23.467-44.8 19.2-9.737-2.434-16.697-9.499-24.401-17.318C110.249 91.543 95.725 76.8 64 76.8z" fill="#38B2AC"/>
+        </svg>
+      </a>
+      <span>
+        tailwindcss
+      </span>
+    </div>
+  </div>
+  <div class="h-screen"></div>
+</main>
