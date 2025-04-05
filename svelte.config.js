3b614a2a6b8b45107ad406aa8590b529ec4fba6c
AhmedHecky <147705662+AhmedHecky@users.noreply.github.com>
2025-04-05 12:11:33 +0000

    added 'fallback' option to svelte.config.js to fix deployment issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default { kit: { adapter: adapter() } };
+export default {
+	kit: {
+		adapter: adapter({
+			fallback: 'app.html'
+		})
+	}
+};
