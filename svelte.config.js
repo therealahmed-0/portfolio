f60552ddab8e81742b61e501d3d0cb00e9adadc6
AhmedHecky <147705662+AhmedHecky@users.noreply.github.com>
2025-04-06 14:36:37 +0000

    changed svelte.config.js to prerender the images

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
       pages: 'build',
-      assets: 'build',
-      fallback: "index.html",
+      assets: 'build'
     }),
-  },
+    prerender: {
+      default: true
+    }
+  }
 };
 
 export default config;
